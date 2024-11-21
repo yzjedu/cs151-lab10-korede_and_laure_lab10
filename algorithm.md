48460d30cab3ef1d5c6be31e0ea7003ba0d8eaf6
LaurePatera <ampatera@loyola.edu>
2024-11-20 22:29:36 -0500

    algorithm added

--- algorithm.md
+++ algorithm.md
@@ -1,5 +1,92 @@
 # Algorithm Document
 
-#### THINK before you code...
-#### Write down the list of tasks to help you think
+* Purpose: get user input of which file to read
+* Name: get_input_filename
+* Parameters: none
+* Return: input_filename, string, represents name of the file that will be read
+* Algorithm:
+1. Set variable input_filename set user to user input, given the output "What filename would you like to read?"
+2. return input_filename
+-------------------------------
+* Purpose: reads the file to a list
+* Name: read_file_to_list
+* Parameters: filename, string, the name of the file that will be read
+* Return: movie_table, table, table of data for each movie from the file
+* Algorithm:
+1. create empty table movie_table
+2. try
+   1. open filename for reading
+   2. for each line in the file
+      1. create a row for each line split along the commas
+      2. add each row to the table movie_table
+      3. convert index 4 of each row to an integer
+      4. convert index 3 of each row to an integer
+      5. convert index 2 of each row to an integer
+   3. Close the file
+3. Except for a filenotfound error
+   1. output to the user "File does not exist"
+4. Return movie_table
+------------------
+* Purpose: get user input of which file to write to
+* Name: get_output_filename
+* Parameters: none
+* Return: output_filename, string, represents name of the file that will be created and written to
+* Algorithm:
+1. set output_filename equal to user input given the output, "What is the name of the file you would like to output?"
+2. return output_filename
+-----------------
+* Purpose: writes the table to a new file
+* Name: write_table_to_file
+* Parameters: movie_table (table, table of data from each movie), output_filename (string, name of file that will be created where the data will be written)
+* Return: none
+* Algorithm:
+1. open output_filename for writing, setting it equal to the variable outfile
+2. for each row in the movie_table
+   1. set profit equal to row index -1 - row index -3
+   2. add a row to the table for profit
+   3. create an empty line
+   4. for each item in row
+      1. add item to the line with a comma separating it
+      2. write each line to the outfile
+3. close outfile
+-----------------
+* Purpose: finds which movie has the highest profit
+* Name: find_highest_profit
+* Parameters: table (table, which table to find the highest profit from)
+* Return: highest_row (string, which row has the highest profit)
+* Algorithm:
+1. Initialize the first row of the table as the highest_row for comparison
+2. for each row in the table
+   1. if index -1 of the row is greater than index -1 of highest_row
+      1. Highest row is set equal to row
+3. Return highest_row
+-----------------
+* Purpose: finds which movie has the lowest profit
+* Name: find_lowest_profit
+* Parameters: table (table, which table to find the lowest profit from)
+* Return: lowest_row (string, which row has the lowest profit)
+* Algorithm:
+1. Initialize the first row of the table as the lowest_row for comparison
+2. for each row in the table
+   1. if index -1 of the row is less than index -1 of lowest_row
+      1. lowest_row is set equal to row
+3. Return lowest_row
+-----------------
+* Purpose: main function
+* Name: main
+* Parameters: none
+* Return: none
+* Algorithm:
+1. Output welcome message and purpose of the program
+2. call get_input_filename and set it equal to input_filename
+3. call read_file_to_list with the argument input_filename and set it equal to movie_table
+4. call get_output_filename and set it equal to output_filename
+5. call write_table_to_file with the arguments movie_table and output_filename
+6. call find_highest_profit with the argument movie_table and set it equal to highest_row
+7. output to the user "The movie with the highest profit is {highest_row}"
+8. call find_lowest_profit with the argument movie_table and set it equal to lowest_row
+9. output to the user "The movie with the lowest profit is {lowest_row}"
+10. output exit message to the user
+-----------------
+1. Call the main function
 
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-def get_filename():
-    input_file_name = input('What filename would you like to read?')
-    return input_file_name
+# Programmers:  Laure Patera and Korede Oni
+# Course:  CS151, Dr. Zee
+# Due Date: 11/21/24
+# Lab Assignment:  Lab 10
+# Problem Statement: analyze data on movies, their budgets, and their profits, creating a new file to update movies.csv with new values and make certain values searchable
+# Data In: input_filename (which file the user would like the program to read), output_filename (which file the user would like the program to write to)
+# Data Out:
+# Credits: Codes from our class repository and the readme file
+# Input Files: must be .csv, movies.csv expected
 
+
+
+# Purpose: get user input of which file to read
+# Parameters: none
+# Return: input_filename, string, represents name of the file that will be read
+def get_input_filename():
+    #asks user which file they would like to read
+    input_filename = input('What filename would you like to read?')
+    return input_filename
+
+
+# Purpose:  reads the file to a list
+# Parameters: filename, string, the name of the file that will be read
+# Return: movie_table, table, table of data for each movie from the file
 def read_file_to_list(filename):
+    #creates empty table
     movie_table = []
-
+    #Tries to open file for reading and adds each line of the file to movie_table
     try:
         file = open(filename, "r")
         for line in file:
@@ -16,21 +37,36 @@ def read_file_to_list(filename):
 
         file.close()  # Close the file after reading
 
+    #If there is an error because the file does not exist, tell the user
     except FileNotFoundError:
         print("File doesn’t exist")
 
+    # return the data of movie_table
     return movie_table
 
 
+# Purpose: get user input of which file to write to
+# Parameters: none
+# Return: output_filename, string, represents name of the file that will be created and written to
+def get_output_filename():
+    #asks user which file they would like to write to
+    output_filename = input('What is the name of the file you would like to output?')
+    return output_filename
+
+
+# Purpose:  writes the table to a new file
+# Parameters: movie_table (table, table of data from each movie), output_filename (string, name of file that will be created where the data will be written)
+# Return: none
 def write_table_to_file(movie_table, output_filename):
     # Open the file for writing
     outfile = open(output_filename, "w")
 
-    # Write each item in the list to the file
     for row in movie_table:
+        #create row for profit, and calculate it by international gross minus the budget
         profit = row[-1] - row[-3]
         row.append(profit)
         line = ''
+        # Write each item in the table to the file
         for item in row:
             line += str(item) + ','
         outfile.write(line + '\n')
@@ -38,39 +74,71 @@ def write_table_to_file(movie_table, output_filename):
     # Close the file
     outfile.close()
 
-def find_highest_profit(movie_table):
-    # Initialize the first actress as the youngest for comparison
-    highest_row = movie_table[0]
 
-    # Iterate through each actress record
-    for row in movie_table:
-        # Check if the current actress is younger than the current youngest
-        if row[-1] > highest_row[-1]:  # Assuming age is at index 3
+# Purpose: finds which movie has the highest profit
+# Parameters: table (table, which table to find the highest profit from)
+# Return: highest_row (string, which row has the highest profit)
+def find_highest_profit(table):
+    # Initialize the first row as the highest profit for comparison
+    highest_row = table[0]
+
+    # Iterate through each movie record
+    for row in table:
+        # Check if the current movie has a higher profit than the current highest
+        if row[-1] > highest_row[-1]:  # Assuming profit is at index -1
             highest_row = row
 
-    # Return the data of the youngest actress
+    # Return the data of the highest row
     return highest_row
 
-def find_lowest_profit(movie_table):
-    # Initialize the first actress as the youngest for comparison
-    lowest_row = movie_table[0]
 
-    # Iterate through each actress record
-    for row in movie_table:
-        # Check if the current actress is younger than the current youngest
-        if row[-1] < lowest_row[-1]:  # Assuming age is at index 3
+# Purpose:  finds which movie has the lowest profit
+# Parameters: table (table, which table to find the lowest profit from)
+# Return: lowest_row (string, which row has the lowest profit)
+def find_lowest_profit(table):
+    # Initialize the first row as the lowest profit for comparison
+    lowest_row = table[0]
+
+    # Iterate through each movie record
+    for row in table:
+        # Check if the current movie has a lower profit than the current lowest
+        if row[-1] < lowest_row[-1]:  # Assuming profit is at index -1
             lowest_row = row
 
-    # Return the data of the youngest actress
+    # Return the data of the lowest row
     return lowest_row
 
+
+# Purpose:  main function
+# Parameters: none
+# Return: none
 def main():
-    input_file_name = get_filename()
-    movie_table = read_file_to_list(input_file_name)
-    write_table_to_file(movie_table, 'movies_updated.csv')
+
+    #Output welcome message and purpose of the program
+    print('Welcome! This program analyzes data on movies from a file. \nAfterwards it calculates profit to a new updated version of the file, and outputs the movies with highest and lowest profits.')
+
+    #call get_input_filename to set input_filename equal to user input
+    input_filename = get_input_filename()
+
+    #read the data from input_filename onto movie_table
+    movie_table = read_file_to_list(input_filename)
+
+    # call get_output_filename to set input_filename equal to user input
+    output_filename = get_output_filename()
+
+    #Write the data from movie_table onto a new file
+    write_table_to_file(movie_table, output_filename)
+
+    #find the movie with the highest profit and output it to the user
     highest_row = find_highest_profit(movie_table)
     print(f'The movie with the highest profit is {highest_row}')
+
+    #find the movie with the lowest profit and output it to the user
     lowest_row = find_lowest_profit(movie_table)
     print(f'The movie with the lowest profit is {lowest_row}')
 
+    #Output exit message to the user
+    print('Thank you for using this program!')
+
+# Call main function
 main()
